b8aba6deb51f9a9fefe531a750ef36cf88ef79df
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-10-02 12:02:12 +0200

    comment

--- Moteur.pas
+++ Moteur.pas
@@ -11,14 +11,13 @@ procedure initialisation(var jeu: TJeu);
 var
     i: integer;
 begin
-    
+    (* Créer une parcelle en mémoire *)
     GetMem(jeu.plateau.parcelles, SizeOf(TParcelle));
 
     jeu.plateau.parcelles[0].px := 0;
     jeu.plateau.parcelles[0].py := 0;
 
     (* Initialise une parcelle vide *)
-
     jeu.plateau.tailleParcelle := 16;
     for i := 1 to jeu.plateau.tailleParcelle do
     begin
